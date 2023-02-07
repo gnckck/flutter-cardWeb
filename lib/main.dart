80052908e5f9491c234802d61c2ac4ea3c3eb458
Jeong Jina <101711121+gnckck@users.noreply.github.com>
2023-02-07 15:13:45 +0900

    카드 위치변경
    < Dragabble >
    - (required) feedback :
    드래그가 진행 중일때 포인터 아래에 표시할 위젯
    - (required) child :
    드래그 하지 않을 때 그려지는 위젯
    
    < DragTarget >
    드래그 된 위젯이 드롭할 대상이 되는 위젯
    
    -----------------------------------
    
    beforeIndex 가 null 일 경우 (막 드래그 됐을 때) 현재 index 를 넣어주고,
    현재 index가 아닌 다른 index로 드래그 했을 경우 beforeIndex를 제거하기 전 temp에 beforeIndex값을 저장해준다.
    그 후 현재 index에 저장해둔 beforeIndex 값을 넣어준 후 beforeIndex에 현재 index를 넣어준다.
    드랍을 했을 경우엔 beforeIndex를 null로 초기화( showCard가 아닌 ListView에서 drop을 할수 있으므로 리스트뷰 dragtarget에서도 null로 초기화 )

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -26,17 +27,21 @@ class CardPage extends StatefulWidget {
   State<CardPage> createState() => _CardPageState();
 }
 
+final cNums = Random();
+
 class _CardPageState extends State<CardPage> {
   final numList = [];
-  final cNums = Random();
 
   @override
   void initState() {
     for (int i = 0; i < 10; i++) {
-      numList.add(cNums.nextInt(50));
+      numList.add(i);
+      //   numList.add(cNums.nextInt(50));
     }
     super.initState();
   }
+
+  dynamic beforeIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -53,37 +58,112 @@ class _CardPageState extends State<CardPage> {
                   })),
               child: const Icon(Icons.add)),
           Expanded(
-            child: ListView.builder(
-                // shrinkWrap: true,         => children이 많아져 화면이상으로 넘어가면 오버플로우가 발생
-                itemCount: numList.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                        title: Text(
-                          numList[index].toString(),
+            child: DragTarget(
+              builder: (context, candidateData, rejectedData) {
+                return ListView.builder(
+                    // shrinkWrap: true,         => children이 많아져 화면이상으로 넘어가면 오버플로우가 발생
+                    itemCount: numList.length,
+                    itemBuilder: (context, index) {
+                      final deviceWidth =
+                          MediaQuery.of(context).size.width; // 현재 디바이스 넓이
+                      final Widget showCard = SizedBox(
+                        width: deviceWidth,
+                        child: Card(
+                          child: ListTile(
+                              title: Text(
+                                numList[index].toString(),
+                              ),
+                              //trailing을 사용하지 않고 title을 GestureDetector로 감싸서
+                              //Icon을 넣어줘도 됨!
+                              trailing: GestureDetector(
+                                child: const Icon(
+                                  Icons.delete,
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    numList.removeAt(index);
+                                  });
+                                },
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  numList[index]++;
+                                });
+                              }),
                         ),
-                        //trailing을 사용하지 않고 title을 GestureDetector로 감싸서
-                        //Icon을 넣어줘도 됨!
-                        trailing: GestureDetector(
-                          child: const Icon(
-                            Icons.delete,
-                          ),
-                          onTap: () {
-                            setState(() {
-                              numList.removeAt(index);
-                            });
+                      );
+
+                      return Draggable<int>(
+                        feedback: showCard,
+                        data: index, // key, context,findRenderObject 검색해보기
+                        child: DragTarget<int>(
+                          builder: (context, candidateData, rejectedData) {
+                            return showCard;
+                          },
+                          onMove: (details) {
+                            beforeIndex ??= index;
+                            if (beforeIndex != index) {
+                              int temp = numList[beforeIndex];
+                              numList.removeAt(beforeIndex);
+                              numList.insert(index, temp);
+                              beforeIndex = index;
+                            }
+                            setState(() {});
+
+                            //   int movingIndex = details.data;
+
+                            //   if (beforeIndex == null) {
+                            //     numList.insert(index, numList[movingIndex]);
+                            //     numList.removeAt(index);
+                            //     movingIndex = index;
+                            //   }
+
+                            //   if (beforeIndex != index) {
+                            //     beforeIndex = movingIndex;
+                            //     numList.removeAt(movingIndex);
+                            //     numList.insert(index, numList[beforeIndex]);
+
+                            //     beforeIndex = index;
+                            //   }
+                          },
+                          onAccept: (data) {
+                            beforeIndex = null;
+                            setState(() {});
                           },
                         ),
-                        onTap: () {
-                          setState(() {
-                            numList[index]++;
-                          });
-                        }),
-                  );
-                }),
+                      );
+                    });
+              },
+              onAccept: (data) {
+                beforeIndex = null;
+
+                setState(() {});
+              },
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+// Feedback
+// cNums나 numList 변수이름 변경
+// onPressed() 와 onTap()에서 어떤 상태로 바뀌는지 바로 알수있게
+
+// Card 위치변경
+// ReorderableListView.builder(
+//                 // shrinkWrap: true,         => children이 많아져 화면이상으로 넘어가면 오버플로우가 발생
+//                 onReorder: (oldIndex, newIndex) {
+//                   setState(() {
+//                     if (newIndex > oldIndex) {
+//                       // 드래그했는지 확인
+//                       newIndex -= 1; // 최신 항목으로 설정
+//                     }
+//                     final temp = numList[oldIndex];
+//                     numList.removeAt(oldIndex);
+//                     numList.insert(newIndex, temp); // 배치해야하는 곳에 newIndex 삽입
+//                   });
+
+//return Card(
+//                  key: Key('$index'),
